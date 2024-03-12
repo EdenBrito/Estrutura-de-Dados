82899687d8459ff5d80d9f2ea017ab6a7ea73264
EdenBrito <161359448+EdenBrito@users.noreply.github.com>
2024-03-12 18:43:06 -0300

    Update Principal.java
    1ª atualização

--- Ordenacao/Lista01/src/view/Principal.java
+++ Ordenacao/Lista01/src/view/Principal.java
@@ -10,23 +10,27 @@ public class Principal {
 		
 		int [] vet1 = {74,20,74,87,81,16,25,99,44,58};
 		int[] vet2 = {44,43,42,41,40,39,38};
-		
+				
 		int opc;
 				
 		opc =Integer.parseInt(JOptionPane.showInputDialog("Escolha o Vetor \n 1 - Vetor 1 por BubbleSort \n 2 - Vetor 2 por BubbleSort \n 3 - Vetor 1 por MergeSort \n 4 - Vetor 2 por MergeSort "));
 		
 		switch (opc) {
-		case 1:
-			System.out.println(ord.bubblesort(vet1));
+		case 1:			
+			vet1 = ord.funbubble(vet1);
+			ord.mostrabb(vet1);
 			break;
-		case 2:
-			System.out.println(ord.bubblesort(vet2));
+		case 2:			
+			vet2 = ord.funbubble(vet2);
+			ord.mostrabb(vet2);
 			break;
 		case 3:
-			System.out.println(ord.mergesort(vet1, 0, 0));
+			vet1 = ord.funmerge(vet1, 0, vet1.length -1);
+			ord.mostramg(vet1, 0, vet1.length -1);
 			break;
-		case 4:
-			System.out.println(ord.mergesort(vet2, 0, 0));
+		case 4:			
+			vet2 = ord.funmerge(vet2, 0, vet2.length -1);
+			ord.mostramg(vet2, 0, vet2.length -1);
 			break;
 		default:
 			System.out.println("Opção Inválida");
